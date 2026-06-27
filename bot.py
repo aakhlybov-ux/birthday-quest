f517b43ad3bab311dcbb0657e9400842872ca1b0
aakhlybov-ux <algebrawhite04@gmail.com>
2026-06-27 21:36:12 +0500

    Width decrease

--- bot.py
+++ bot.py
@@ -64,7 +64,7 @@ async def cmd_start(message: types.Message):
     kb = [
         [types.KeyboardButton(
             text="Пройти опрос 🎁", 
-            web_app=types.WebAppInfo(url="https://aakhlybov-ux.github.io/birthday-quest/?v=8")
+            web_app=types.WebAppInfo(url="https://aakhlybov-ux.github.io/birthday-quest/?v=5")
         )]
     ]
     keyboard = types.ReplyKeyboardMarkup(
